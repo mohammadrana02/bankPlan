2b8777a174f953238274cde9bf2a00fe7effaa5b
mohammadrana02 <mohamamadrana02@gmail.com>
2024-10-03 11:31:12 +0100

    full functionality of the transfer money option

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd # pandas will be used of data manipulation and file handling
 pd.set_option('display.max_columns', None) # so the user's data won't be truncated when it is printed to the console
 
 def login_screen():
@@ -16,7 +16,7 @@ def login_screen():
             print('Username or password incorrect.') # if they don't match
 
 def admin_screen():
-    print('Welcome to the admin dashboard.')
+    print('Welcome to the admin dashboard.') # admin dashboard with a number of options
     print('1. Customer search.')
     print('2. Update admin information.')
     print('3. Print all customer details')
@@ -40,13 +40,13 @@ def admin_screen():
 
 def customer_options(account_number):
     while True:
-        print('Welcome to the customer options screen.')
+        print('Welcome to the customer options screen.') # accessed through the admin dashboard
         print('1. Deposit money into account.')
         print('2. Withdraw money from account.')
         print('3. Check account balance.')
         print('4. View customer details.')
         print('5. Update customer information.')
-        print('6. Close customer account')
+        print('6. Close the customer account')
         print('7. transfer money to another account')
         print('8. Logout')
         option = input('What would you like to do? ')
@@ -54,39 +54,48 @@ def customer_options(account_number):
             deposit = int(input("Enter amount to deposit: "))
             df = pd.read_csv('users.csv')
             #df.loc[df['column_condition'] == value_to_match, 'column_to_update'] = new_value
-            df.loc[df['account_number'] == account_number, 'balance'] += deposit
+            df.loc[df['account_number'] == account_number, 'balance'] += deposit # deposits the money into the balance column
             df.to_csv('users.csv', index=False)
             print(f'Deposit successful.')
 
         elif option == '2':
             withdraw = int(input("Enter amount to withdraw: "))
             df = pd.read_csv('users.csv')
-            df.loc[df['account_number'] == account_number, 'balance'] -= withdraw
+            df.loc[df['account_number'] == account_number, 'balance'] -= withdraw # withdraws money from the balance column
             df.to_csv('users.csv', index=False)
             print(f'Withdraw successful.')
 
         elif option == '3':
             df = pd.read_csv('users.csv')
             current_balance = df.loc[df['account_number'] == account_number, 'balance']
-            print(f'Current balance: {current_balance}') # fix output on this one
+            print(f'Current balance: {current_balance}') # fix output on this one, not in a clean format
 
         elif option == '4':
             df = pd.read_csv('users.csv')
             target_user = df[(df['account_number'] == account_number)]
 
+            # looks for the corrects user and outputs all of their information in a clean format
             if not target_user.empty:
                 for index, row in target_user.iterrows():
                     for col in df.columns:
                         print(f"{col}: {row[col]}")
                 else:
-                    print("No matching row found.")
+                    print("No matching row found.") # if the user can't be found
 
         elif option == '5':
             break
-        elif option == '6':
-            break
+        elif option == '6': # drops the row where there account number is, thereby closing the account
+            df = pd.read_csv('users.csv')
+            df.drop(df[df['account_number'] == account_number].index, inplace=True)
+
         elif option == '7':
-            break
+            transfer_to = int(input("Enter the account number you want to transfer to: "))
+            transfer_money = int(input("Enter the amount to transfer money: "))
+            df = pd.read_csv('users.csv')
+            df.loc[df['account_number'] == account_number, 'balance'] -= transfer_money
+            df.loc[df['account_number'] == transfer_to, 'balance'] += transfer_money
+            df.to_csv('users.csv', index=False)
+            print(f'Transfer successful.')
         elif option == '8':
             login_screen()
         else:
@@ -98,7 +107,7 @@ def update_admin_info():
 def customer_details():
     df = pd.read_csv('users.csv')
 
-    for index, row in df.iterrows():
+    for index, row in df.iterrows(): # outputs all the customer details in an ordered format
         for col in df.columns:
             print(f"{col}: {row[col]}")
         print()
